59264312c22984e34b36e53d714133ece50a01ea
ameri <ameri@host.docker.internal>
2023-08-17 16:39:41 -0300

    modificadores de acesso
    e métodos getters e setters

--- src/main/java/simulador/code/Robo.java
+++ src/main/java/simulador/code/Robo.java
@@ -5,18 +5,18 @@ package simulador.code;
  * @author americo relvas
  */
 public class Robo {
-    float posicaoX;
-    float posicaoY;
-    final String nome;
-    final float peso;
-    final float velocidadeMax = 5;
-    final float pesoCargaMax = 20;
-    final String tipoTracao = "esteira";
-    int orientacao;
-    static final int FRENTE = 0;
-    static final int ATRAS = 1;
-    static final int ESQUERDA = 2;
-    static final int DIREITA = 3;
+    private float posicaoX;
+    private float posicaoY;
+    private int orientacao;
+    private final String nome;
+    private final float peso;
+    private final float velocidadeMax = 5;
+    private final float pesoCargaMax = 20;
+    private final String tipoTracao = "esteira";
+    public static final int FRENTE = 0;
+    public static final int ATRAS = 1;
+    public static final int ESQUERDA = 2;
+    public static final int DIREITA = 3;
     public Robo(String nome, float peso){
         this.nome = nome;
         this.peso = peso;
@@ -36,6 +36,29 @@ public class Robo {
         this.posicaoX = posX;
         this.posicaoY = posY;
     }
+    public float getPosicaoX() {
+        return posicaoX;
+    }
+    public float getPosicaoY() {
+        return posicaoY;
+    }
+    public int getOrientacao() {
+        return orientacao;
+    }
+    @Override
+    public String toString() {
+        return "Robo{" + "posicaoX=" + posicaoX + ", posicaoY="
+               + posicaoY + ", orientacao=" + orientacao + '}';
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Robo){
+            Robo robo = (Robo)obj;
+            return this.nome.equals(robo.nome);
+        } else {
+            return false;
+        }
+    }
     public void setOrientacao(char tecla){
         if (tecla == 'w') {
             this.orientacao = FRENTE;
@@ -50,4 +73,5 @@ public class Robo {
     public void printPos() {
         System.out.println("Posição do Robô: (" + posicaoX + ", " + posicaoY + ")");
     }
+    
 }
